5dbd960bcb8e4b7c83199cb2f7bf0797782acd7f
MenilikBelay <menilikbelay@gmail.com>
2020-04-14 22:01:24 -0500

    fix issues with dependency/gitignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,27 @@
-import { EditProfileComponent } from './edit-profile/edit-profile.component';
-import { AuthInterceptor } from './auth/auth-interceptor';
-import { BrowserModule } from '@angular/platform-browser';
-import { MatInputModule } from '@angular/material/input';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatRadioModule} from '@angular/material/radio';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { EditProfileComponent } from "./edit-profile/edit-profile.component";
+import { AuthInterceptor } from "./auth/auth-interceptor";
+import { BrowserModule } from "@angular/platform-browser";
+import { MatInputModule } from "@angular/material/input";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatRadioModule } from "@angular/material/radio";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
-import { NgModule } from '@angular/core';
-import {FormsModule,ReactiveFormsModule} from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { TopbarComponent } from './topbar/topbar.component';
-import { LeftsidebarComponent } from './leftsidebar/leftsidebar.component';
-import { RightsidebarComponent } from './rightsidebar/rightsidebar.component';
-import { CenterlayoutComponent } from './centerlayout/centerlayout.component';
-import { PostCreateComponent } from './post-create/post-create.component';
-import { PostListComponent } from './post-list/post-list.component';
-import { CommentsComponent } from './comments/comments.component';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './auth/login_singup/login.component';
-import { AppRoutingModule } from './app-routing.module';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { TopbarComponent } from "./topbar/topbar.component";
+import { LeftsidebarComponent } from "./leftsidebar/leftsidebar.component";
+import { RightsidebarComponent } from "./rightsidebar/rightsidebar.component";
+import { CenterlayoutComponent } from "./centerlayout/centerlayout.component";
+import { PostCreateComponent } from "./post-create/post-create.component";
+import { PostListComponent } from "./post-list/post-list.component";
+import { CommentsComponent } from "./comments/comments.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./auth/login_singup/login.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [
@@ -36,8 +35,7 @@ import { AppComponent } from './app.component';
     CommentsComponent,
     HomeComponent,
     LoginComponent,
-    EditProfileComponent
-
+    EditProfileComponent,
   ],
   imports: [
     BrowserModule,
@@ -49,9 +47,11 @@ import { AppComponent } from './app.component';
     MatRadioModule,
     HttpClientModule,
     ReactiveFormsModule,
-    MatPaginatorModule
+    MatPaginatorModule,
   ],
-  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
